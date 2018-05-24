2e1f92fe2618bcd6e09f0add80846f2cda06c934
Burdis <andy.burdis@yahoo.com>
2018-05-24 15:52:14 +0100

    TEST-1: I updated the code

--- src/test/java/stepdefs/StepDefs.java
+++ src/test/java/stepdefs/StepDefs.java
@@ -1,6 +1,7 @@
 package stepdefs;
 
-import cucumber.api.java8.En;
+import com.sun.istack.internal.NotNull;
+import cucumber.api.java.en.Given;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,21 +9,15 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.Driver;
 
-import static org.junit.Assert.assertEquals;
 
-public class StepDefs implements En{
+public class StepDefs {
 
     private static WebDriver driver = Driver.getDriver();
     private static WebDriverWait wait = new WebDriverWait(driver, 5);
 
-    public StepDefs(){
+    @Given("^test$")
+    public void test() {
 
-        Given("^I go to my selected site$", () -> {
-
-        });
-
-        Then("^I will be on (.*)$", (String title) -> {
-
-        });
     }
+
 }
